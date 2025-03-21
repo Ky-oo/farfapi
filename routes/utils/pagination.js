cbf6ba53a457c70f0d92af8ccb59bd6017d7f40e
Kylian <kylian.patry@gmail.com>
2025-03-21 23:10:40 +0100

    Simplify pagination

--- routes/utils/pagination.js
+++ routes/utils/pagination.js
@@ -1,42 +1,29 @@
 const { Op } = require("sequelize");
 
 async function handlePagination(req, model) {
-  const nbDisplayed = req.query.pagination;
+  const nbDisplayed = parseInt(req.query.pagination);
   const pages = parseInt(req.query.pages) - 1 || 0;
 
   const quantity = await model.count();
+  if (quantity === 0) return { error: "Collection is empty" };
 
-  if (quantity == 0) {
-    return { error: "Collection is empty" };
+  if (!nbDisplayed || isNaN(nbDisplayed) || nbDisplayed < 1) {
+    return { error: "Pagination must be a valid number greater than 0" };
   }
 
-  if (nbDisplayed) {
-    if (isNaN(nbDisplayed)) {
-      return { error: "Pagination must be a number" };
-    }
+  if (pages < 0)
+    return { error: "Pages must be a valid number greater than 0" };
 
-    if (parseInt(nbDisplayed) < 1) {
-      return { error: "Pagination not valid" };
-    }
-
-    if (parseInt(pages) < 0) {
-      return { error: "Pages not valid" };
-    }
-
-    const totalPages = Math.ceil(quantity / parseInt(nbDisplayed));
-
-    if (pages >= totalPages) {
-      return { error: `Page not found, max page is ${totalPages}` };
-    }
-
-    return {
-      limit: parseInt(nbDisplayed),
-      offset: pages * parseInt(nbDisplayed),
-      totalPages,
-    };
+  const totalPages = Math.ceil(quantity / nbDisplayed);
+  if (pages >= totalPages) {
+    return { error: `Page not found, max page is ${totalPages}` };
   }
 
-  return { limit: null, offset: null, totalPages: null };
+  return {
+    limit: nbDisplayed,
+    offset: pages * nbDisplayed,
+    totalPages,
+  };
 }
 
 module.exports = handlePagination;
