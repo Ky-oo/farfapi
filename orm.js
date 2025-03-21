25d804a2b231bbfb183122995be4d8ab81b2edc8
Kylian <kylian.patry@gmail.com>
2025-03-22 00:43:00 +0100

    change connection to db to uri

--- orm.js
+++ orm.js
@@ -1,10 +1,5 @@
 const Sequelize = require("sequelize");
 
-const sequelizeInstance = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  { host: process.env.DB_HOST, dialect: "mysql" }
-);
+const sequelizeInstance = new Sequelize(process.env.DB_URI);
 
 module.exports = sequelizeInstance;
