2d903caa6ee57da0c09782e7be35cc4d60b42a08
Kylian <kylian.patry@gmail.com>
2025-03-30 22:06:40 +0200

    fix pagination bugs

--- routes/utils/pagination.js
+++ routes/utils/pagination.js
@@ -1,19 +1,19 @@
 const { Op } = require("sequelize");
 
 async function handlePagination(req, model) {
-  const nbDisplayed = parseInt(req.query.pagination);
-  const pages = parseInt(req.query.pages) - 1 || 0;
+  let nbDisplayed = parseInt(req.query.pagination);
+  let pages = parseInt(req.query.pages) - 1 || 0;
 
   let quantity = await model.count();
   quantity = quantity === 0 ? 1 : quantity;
 
   if (!nbDisplayed || isNaN(nbDisplayed) || nbDisplayed < 1) {
-    return { error: "Pagination must be a valid number greater than 0" };
+    nbDisplayed = 1;
   }
 
-  if (pages < 0)
-    return { error: "Pages must be a valid number greater than 0" };
-
+  if (pages < 0) {
+    page = 1;
+  }
   const totalPages = Math.ceil(quantity / nbDisplayed);
   if (pages >= totalPages) {
     return { error: `Page not found, max page is ${totalPages}` };
